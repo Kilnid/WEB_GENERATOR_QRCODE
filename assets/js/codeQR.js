d545f53d0dd5123304f7df63296e3ced123c3957
Kiln02 id <ide.kilian02@gmail.com>
2022-04-26 15:52:24 +0200

    séparation wifi & url en deux pages, url fonctionnel

--- assets/js/codeQR.js
+++ assets/js/codeQR.js
@@ -1,26 +1,34 @@
+//URL
+
 //Valeur de l'URL a mettre en QRCODE
-var QRCode_URL = Object;
 
-
-
-new QRCode(document.getElementById("displayqrcode"), "3URL_QR3");
-
-
-
-
-
-
-function updateQRCode() {
-    var ssid = document.getElementById("ssid").value;
-    var pw = document.getElementById("password").value;
-    var enc = document.getElementById("enc").value;
-    var hidden = document.getElementById("hidden").checked;
-    var qrcode = document.getElementById("qrcode");
-
-    var text =
-        "WIFI:S:" + ssid + ";T:" + enc + ";P:" + pw + ";H:" + hidden + ";;";
-
-    qrcode.replaceChild(showQRCode(text), qrcode.lastChild);
+var displayqrcode = document.getElementById("displayqrcode");
+var url = document.getElementById("url");
+function urlQrCodeGenerator() {
+    displayqrcode.innerHTML = "";
+    var valeururl = url.value;
+    new QRCode(displayqrcode, valeururl);
+    console.log(url);
 }
 
-document.getElementById("form").onchange = updateQRCode;
+
+
+
+//WIFI
+
+function wifiQrCodeGenerator() {
+    function updateQRCode() {
+        var ssid = document.getElementById("ssid").value;
+        var pw = document.getElementById("password").value;
+        var enc = document.getElementById("enc").value;
+        var hidden = document.getElementById("hidden").checked;
+        var qrcode = document.getElementById("qrcode");
+
+        var text =
+            "WIFI:S:" + ssid + ";T:" + enc + ";P:" + pw + ";H:" + hidden + ";;";
+
+        qrcode.replaceChild(showQRCode(text), qrcode.lastChild);
+    }
+
+    document.getElementById("form").onchange = updateQRCode;
+}
